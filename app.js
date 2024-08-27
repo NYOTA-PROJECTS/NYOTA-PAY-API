97673380428ae774092df54e1ff736c006de4123
rubensalban <rubensalban@gmail.com>
2024-08-28 00:24:26 +0100

    Init

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const fs = require("fs");
+const admin = require("firebase-admin");
+const permissionsPolicy = require("permissions-policy");
+const serviceAccount = require("./utils/firebase.json");
+
+// Init express app
+const app = express();
+
+// Firebase cloud messaging initialisation
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// Logger
+app.use(morgan("tiny"));
+
+// Init dotenv
+dotenv.config();
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware to create the destination folder for public
+const createUploadsProfileFolder = (req, res, next) => {
+  const folderPath = "public/merchants";
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+  }
+  next();
+};
+
+// Public directory
+app.use(express.static("public"));
+
+// Implement security measures
+// XSS protection
+app.use(helmet.xssFilter());
+app.use(helmet.frameguard({ action: "sameorigin" }));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.referrerPolicy({ policy: "same-origin" }));
+app.use(helmet.noSniff());
+app.use(helmet());
+
+// CSP protection
+app.use(
+  helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"] } })
+);
+
+// Permissions policy
+app.use(
+  permissionsPolicy({
+    features: {
+      payment: ["self", '"nyota-apps.com"'],
+      syncXhr: [],
+    },
+  })
+);
+
+// Routes
+
+// Export app
+const port = process.env.PORT || 3000;
+
+// Start server
+app.listen(port, () => {
+  console.log(`
+  ==================================
+  |🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀|
+  |===== API NYOTA PAY MOBILE =====|
+  |======= ENV: ${process.env.NODE_ENV} =======|
+  |========== POST: ${process.env.PORT} ==========|
+  |🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀|
+  ==================================
+  `);
+});
---
+++ migrations/20240827230755-create-admin.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Admins', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      photo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'https://avatar.iran.liara.run/public/11'
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Admins');
+  }
+};
