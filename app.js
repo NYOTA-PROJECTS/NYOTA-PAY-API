6ac38d8ef4033fb3951aa21978e9b24b8c4f1d19
rubensalban <rubensalban@gmail.com>
2024-09-20 03:35:16 +0100

    Suppréssion SOCKET.IO

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-//const http = require("http");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
@@ -18,20 +17,9 @@ const pointofsellRoutes = require("./routes/pointofsellRoutes");
 const workerRoutes = require("./routes/workerRoutes");
 const customers = require("./routes/customerRoutes");
 const transactions = require("./routes/transactionRoutes");
-//const { getCashRegisterBalance } = require("./controllers/workerController");
-//const { Server } = require("socket.io");
 
 // Init express app
 const app = express();
-
-/* // Créer un serveur HTTP pour utiliser avec Socket.IO
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*", // Remplacez par votre URL si besoin
-    methods: ["GET", "POST"],
-  },
-}); */
 
 // Firebase cloud messaging initialisation
 admin.initializeApp({
@@ -99,36 +87,6 @@ app.use("/api/v1/worker", workerRoutes);
 app.use("/api/v1/customer", createUploadsCustomerFolder, customers);
 app.use("/api/v1/transaction", transactions);
 
-// Socket.IO : Gestion des utilisateurs connectés
-// Socket.IO : gestion des connexions
-/* io.on("connection", (socket) => {
-  console.log("Un utilisateur est connecté");
-  socket.on("getWorkerBalance", async (token, cashRegisterId) => {
-    try {
-      // Utiliser "await" pour attendre la résolution de la promesse avant de continuer
-      const balance = await getCashRegisterBalance(token, cashRegisterId); 
-      // Si le solde est récupéré avec succès, l'envoyer au client
-      if (balance !== null) {
-        socket.emit("workerBalance", { balance });
-      } else {
-        socket.emit("workerBalanceError", { message: "Impossible de récupérer le solde." });
-      }
-    } catch (error) {
-      console.error(`Erreur lors de la récupération du solde: ${error}`);
-      socket.emit("workerBalanceError", { message: "Erreur serveur lors de la récupération du solde." });
-    }
-  });
-  socket.on("disconnect", () => {
-    console.log("Un utilisateur est déconnecté");
-  });
-});
-
-server.listen(6881, () => {
-  console.log("Le serveur est démarré sur le port 3000");
-}); */
-console.log("Twilio Account SID:", process.env.TWILIO_ACCOUNT_SID);
-console.log("Twilio Auth Token:", process.env.TWILIO_AUTH_TOKEN);
-console.log("Twilio Phone Number:", process.env.TWILIO_PHONE_NUMBER);
 // Export app
 const port = process.env.PORT || 3000;
 
